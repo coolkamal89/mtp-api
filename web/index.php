d0b143a892717dbe72cb7324f4ba09c64137de49
Kamal Relwani <coolkamal89@yahoo.co.in>
2018-04-11 01:30:24 +0530

    Update index.php

--- web/index.php
+++ web/index.php
@@ -19,6 +19,7 @@
 		curl_setopt($ch, CURLOPT_HEADER, 0);
 		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_NOSIGNAL, 1);
 		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 		curl_setopt($ch, CURLOPT_TIMEOUT, 2);
 
