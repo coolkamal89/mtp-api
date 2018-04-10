0d512ed9bc315a58e0bedcbbfcd08d35a7467b7b
Kamal Relwani <coolkamal89@yahoo.co.in>
2018-04-11 01:28:29 +0530

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,8 +1,4 @@
 <?php
-
-	error_reporting(E_ALL);
-	ini_set("display_errors", true);
-
 	function getChallanDetails($vehicleNo) {
 
 		$vehicleN = trim(strtoupper($vehicleNo));
@@ -17,16 +13,7 @@
 
 		$ch = curl_init();
 
-		$header[] = "Accept: text/xml,application/xml,application/xhtml+xml,text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5";
-		$header[] = "Cache-Control: max-age=0";
-		$header[] = "Connection: keep-alive";
-		$header[] = "Keep-Alive: 300";
-		$header[] = "Accept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.7";
-		$header[] = "Accept-Language: en-us,en;q=0.5";
-		$header[] = "Pragma: ";
-
-		// curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36");
-		curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+		curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36");
 		curl_setopt($ch, CURLOPT_URL, "http://mumbaipolice.in.net/payechallan/PaymentService.htm");
 		curl_setopt($ch, CURLOPT_COOKIEJAR, __DIR__ . "/cookies/" . $vehicleNo . ".txt");
 		curl_setopt($ch, CURLOPT_HEADER, 0);
@@ -34,11 +21,6 @@
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 		curl_setopt($ch, CURLOPT_TIMEOUT, 2);
-
-		curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.15) Gecko/20080623 Firefox/2.0.0.15');
-		curl_setopt($ch, CURLOPT_REFERER, 'http://www.google.com');
-		curl_setopt($ch, CURLOPT_ENCODING, 'gzip,deflate');
-		curl_setopt($ch, CURLOPT_AUTOREFERER, true);
 
 		$output = curl_exec($ch);
 		$curl_getinfo = curl_getinfo($ch);
